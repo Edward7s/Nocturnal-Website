14a09e6528f32219f4b00bdae827f473ed5eb014
Edward7 <74219635+Edward7s@users.noreply.github.com>
2022-07-18 21:18:51 +0300

    Add files via upload

---
+++ Nocturnal.js
@@ -0,0 +1,16 @@
+
+
+function setup()
+{
+
+document.getElementById("DiscordLink").onclick = function() {window.open('https://discord.nocturnal-client.xyz/')};
+document.getElementById("GitHubLink").onclick = function() {window.open('https://github.com/Edward7s/Nocturnal-V3')};
+document.getElementById("DownloadLink").onclick = function() {window.open('https://nocturnal-client.xyz/Resources/Nocturnal%20Loader.dll')};
+document.getElementById("DiscordContact").onclick = function() {window.open('https://discord.com/users/897972640805949460')};
+document.getElementById("DiscordServerContact").onclick = function() {window.open('https://discord.nocturnal-client.xyz/')};
+document.getElementById("GitHubContact").onclick = function() {window.open('https://github.com/Edward7s')};
+document.getElementById("DiscordHeader").onclick = function() {window.open('https://github.com/Edward7s')};
+document.getElementById("GitHubHeader").onclick = function() {window.open('https://github.com/Edward7s')};
+document.getElementById("DownloadHeader").onclick = function() {window.open('https://nocturnal-client.xyz/Resources/Nocturnal%20Loader.dll')};
+
+}
